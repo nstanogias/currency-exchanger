a82735b401e3c1abd30d3cc13ea6afbdf1bfd206
nstanogias <nstanogias@gmail.com>
2022-12-05 16:34:24 +0100

    added auth interceptor

---
+++ src/app/shared/auth.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const apiKeyReq = request.clone({
+      headers: request.headers.set('apikey', environment.apiUrl),
+    });
+    return next.handle(apiKeyReq);
+  }
+}
