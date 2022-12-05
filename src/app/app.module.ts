2315b4730f2da50e663c5bef4501ef012687b962
nstanogias <nstanogias@gmail.com>
2022-12-05 23:09:30 +0100

    home impl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DetailsComponent } from './components/details/details.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './shared/auth.interceptor';
 
 @NgModule({
@@ -25,6 +31,11 @@ import { AuthInterceptor } from './shared/auth.interceptor';
     HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [
     {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { take } from 'rxjs';
+import { ApiService } from 'src/app/shared/api.service';
+import { ISymbol } from 'src/app/shared/models/symbol';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public amount = 25;
+  public currToCurr = '';
+  public result = '';
+  public currencyForm: FormGroup = this.fb.group({
+    from: ['EUR'],
+    to: ['USD'],
+  });
 
+  public symbols: string[] = [];
+  public constructor(private apiService: ApiService, private fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.apiService
+      .getSymbols()
+      .pipe(take(1))
+      .subscribe((val: ISymbol) => (this.symbols = Object.keys(val.symbols)));
+
+    this.apiService
+      .getConversion(
+        this.currencyForm.get('from')?.value,
+        this.currencyForm.get('to')?.value,
+        1
+      )
+      .pipe(take(1))
+      .subscribe((val) => {
+        this.currToCurr = `1.00 ${this.currencyForm.get('from')?.value} = ${
+          val.result
+        } ${this.currencyForm.get('to')?.value}`;
+      });
+  }
+
+  public swap(): void {
+    const temp = this.currencyForm.get('from')?.value;
+    this.currencyForm.setValue({
+      from: this.currencyForm.get('to')?.value,
+      to: temp,
+    });
+  }
+
+  public convert(): void {
+    this.apiService
+      .getConversion(
+        this.currencyForm.get('from')?.value,
+        this.currencyForm.get('to')?.value,
+        this.amount
+      )
+      .pipe(take(1))
+      .subscribe((val) => (this.result = val.result.toFixed(2).toString()));
+  }
 }
