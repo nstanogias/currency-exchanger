ebf3fa6a06dea9c5e0a27eb8b57a0c625044d1bc
nstanogias <nstanogias@gmail.com>
2022-12-05 23:09:08 +0100

    added conversion api

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IConversion } from './models/conversion';
 import { ISymbol } from './models/symbol';
 
 @Injectable({
@@ -11,5 +12,15 @@ export class ApiService {
 
   public getSymbols(): Observable<ISymbol> {
     return this.client.get<ISymbol>('https://api.apilayer.com/fixer/symbols');
+  }
+
+  public getConversion(
+    from: string,
+    to: string,
+    amount: number
+  ): Observable<IConversion> {
+    return this.client.get<IConversion>(
+      `https://api.apilayer.com/fixer/convert?to=${to}&from=${from}&amount=${amount}`
+    );
   }
 }
---
+++ src/app/shared/models/conversion.ts
@@ -0,0 +1,14 @@
+export interface IConversion {
+  date: string;
+  info: {
+    rate: number;
+    timestamp: number;
+  };
+  query: {
+    amount: number;
+    from: string;
+    to: string;
+  };
+  result: number;
+  success: boolean;
+}
